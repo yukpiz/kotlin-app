32a01e1004df01d6c96158a10eae9cdf37995a26
yukpiz <yukpiz@gmail.com>
2017-07-14 17:44:59 +0900

    Types variable.

---
+++ types.kts
@@ -0,0 +1,12 @@
+// build-inの数値型に対応する
+
+// 合法
+var id: Number = 111
+var long: Long = 111
+var int: Int = 111
+var short: Short = 111
+var byte: Byte = 111
+
+// 違法
+var double: Double = 111
+var float: Float = 111
